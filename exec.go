287a232e84c781e32956c8192204d158b3a040e9
Gai Lazar <gail@jfrog.com>
2021-06-30 12:41:19 +0300

    Remove RunWithFallbacksAndPublish

--- exec.go
+++ exec.go
@@ -2,13 +2,8 @@ package gocmd
 
 import (
 	"github.com/jfrog/gocmd/executers"
-	"github.com/jfrog/gocmd/params"
 )
 
-func RunWithFallbacksAndPublish(goArg []string, noRegistry, publishDeps bool, resolverDeployer *params.ResolverDeployer) error {
-	return executers.RunWithFallbacksAndPublish(goArg, noRegistry, publishDeps, resolverDeployer)
-}
-
-func RunWithFallback(goArg []string, url string) error {
-	return executers.RunWithFallback(goArg, url)
+func RunWithFallback(goArg []string) error {
+	return executers.RunWithFallbacks(goArg)
 }
--- executers/fallbackandpublish.go
+++ executers/fallbackandpublish.go
@@ -1,48 +1,10 @@
 package executers
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/jfrog/gocmd/cmd"
-	"github.com/jfrog/gocmd/executers/utils"
-	"github.com/jfrog/gocmd/params"
-	"github.com/jfrog/jfrog-client-go/utils/errorutils"
-	"github.com/jfrog/jfrog-client-go/utils/log"
 )
 
 // Runs Go, with multiple fallbacks if needed and publish missing dependencies to Artifactory
-func RunWithFallbacksAndPublish(goArg []string, noRegistry, publishDeps bool, resolverDeployer *params.ResolverDeployer) error {
-	if !noRegistry {
-		resolver := resolverDeployer.Resolver()
-		if resolver == nil || resolver.IsEmpty() {
-			return errorutils.CheckError(fmt.Errorf("Missing resolver information"))
-		}
-		artDetails := resolver.ServiceManager().GetConfig().GetServiceDetails()
-		err := utils.SetGoProxyWithApi(resolverDeployer.Resolver().Repo(), artDetails)
-		if err != nil {
-			return err
-		}
-	}
-
-	err := cmd.RunGo(goArg)
-
-	if err != nil {
-		if utils.DependencyNotFoundInArtifactory(err, noRegistry) {
-			log.Info("Received", err.Error(), "from Artifactory. Trying to download dependencies from VCS...")
-			err := os.Unsetenv(utils.GOPROXY)
-			if err != nil {
-				return err
-			}
-
-			err = collectDependenciesAndPublish(true, publishDeps, &Package{}, resolverDeployer)
-			if err != nil {
-				return err
-			}
-			return cmd.RunGo(goArg)
-		} else {
-			return err
-		}
-	}
-	return nil
+func RunWithFallbacks(goArg []string) error {
+	return cmd.RunGo(goArg)
 }
