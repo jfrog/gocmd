2305854634121cc0867d17c79f7a01e87afbc040
sverdlov93 <sverdlov93@gmail.com>
2021-10-19 17:48:29 +0300

    prompt log error for failed go command

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -211,7 +211,7 @@ func runDependenciesCmd(projectDir string, commandArgs []string) (string, error)
 	}
 	if executionError != nil {
 		// If the command fails, the mod stays the same, therefore, don't need to be restored.
-		errorString := fmt.Sprintf("Running 'go %s' in %s with error: '%s'", strings.Join(commandArgs, " "), projectDir, executionError.Error())
+		errorString := fmt.Sprintf("Failed running Go command: 'go %s' in %s with error: '%s'", strings.Join(commandArgs, " "), projectDir, executionError.Error())
 		return "", errorutils.CheckError(errors.New(errorString))
 	}
 
